25a78eefa6ac4bd1cdaf7f48ef76991a74c2a0ce
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-10-19 19:03:58 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,11 +3,11 @@
 def display_board
   puts "Welcome to Tic Tac Toe"
   
-  puts "   |  |  "
+  puts "   |   |   "
   puts "-----------"
-  puts "  |  |  "
+  puts "   |   |   "
   puts "-----------"
-  puts "  |  |  "
+  puts "   |   |   "
 end
 
 display_board
